1323024f3e2d59c8ff339a3a12f30ab87fe6d035
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-21 15:40:27 +0100

    Added "HSL" to enable multi-colored drawing

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ const ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
-ctx.lineWidth = 100;
+ctx.lineWidth = 20;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
 
   ctx.moveTo(lastX, lastY);
@@ -25,6 +28,12 @@ function draw(e) {
   ctx.stroke();
 
   [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue++;
+
+  if (hue >= 360) {
+    hue = 0;
+  }
 }
 
 canvas.addEventListener("mousedown", (e) => {
